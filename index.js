413f1a389a031a9ef902a0f03b58ea4429a3c87d
Daniel Wagener <daniel.j.wagener@gmail.com>
2020-01-15 16:24:58 -0500

    Instruct Express to use cookies

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
 require("./models/User");
 require("./services/passport");
 const keys = require("./config/keys");
@@ -10,6 +13,15 @@ mongoose.connect(keys.mongoURI, {
 });
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
