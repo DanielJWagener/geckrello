240f665b706cb6a6398eabdf64c752e78063a362
Daniel Wagener <daniel.j.wagener@gmail.com>
2020-01-15 09:13:40 -0500

    Heroku deployment test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there " });
+  res.send({ hello: "there" });
 });
 
 const PORT = process.env.PORT || 5000;
