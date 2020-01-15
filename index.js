e1c9e5442a9424af56bbbd6173ab2c5650069aa7
Daniel Wagener <daniel.j.wagener@gmail.com>
2020-01-15 15:37:52 -0500

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("./services/passport");
+const keys = require("./config/keys");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
