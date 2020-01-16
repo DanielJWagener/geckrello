f05a3c8a1f3374bd80f4ead1cd0b39a19b7d4cb4
Daniel Wagener <daniel.j.wagener@gmail.com>
2020-01-16 10:14:54 -0500

    Remove debug statement

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
-// keys.js - figure out whether to show prodction or dev credentials
+// keys.js - figure out whether to show production or dev credentials
 if (process.env.NODE_ENV === "production") {
   // we are in production - return prod keys
-  console.log("in prod");
   module.exports = require("./prod");
 } else {
   // we are in development - return dev keys
