f9a37051d7e6afdc8aa87859d39b4aa54f21aaf0
Daniel Wagener <daniel.j.wagener@gmail.com>
2020-01-16 13:33:49 -0500

    Set up proxy in React layer to redirect /api and /auth routes to Express

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Board from "../board/Board";
-import Nav from "../navbar/NavBar";
+import NavBar from "../navbar/NavBar";
 
 class App extends React.Component {
   constructor(props) {
@@ -12,7 +12,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Nav />
+        <NavBar />
         <Board />
       </div>
     );
--- client/src/components/navbar/Logo.js
+++ client/src/components/navbar/Logo.js
@@ -4,6 +4,7 @@ const logo = () => {
   return (
     <div className="logo navbar__logo">
       <h1>Geckorello</h1>
+      <a href="/auth/google">Sign In With Google</a>
     </div>
   );
 };
