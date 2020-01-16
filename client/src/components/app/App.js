47a6c567973d55a7c0752ea53b86330583c95163
Daniel Wagener <daniel.j.wagener@gmail.com>
2020-01-16 17:36:52 -0500

    Scaffold React Router

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
 import Board from "../board/Board";
 import Navbar from "../navbar/NavBar";
+const Dashboard = () => <h2>Dashboard</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 class App extends React.Component {
   constructor(props) {
@@ -12,7 +16,13 @@ class App extends React.Component {
     return (
       <div className="App">
         <Navbar />
-        <Board />
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={Landing} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/board" component={Board} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
