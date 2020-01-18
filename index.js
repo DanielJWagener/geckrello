6110e62dfead5f718e3eaf3f2b2d171fcf7ce070
Daniel Wagener <daniel.j.wagener@gmail.com>
2020-01-18 17:47:22 -0500

    Instruct Express to handle static files

--- index.js
+++ index.js
@@ -25,5 +25,16 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express will serve up index.html if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
