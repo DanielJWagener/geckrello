e578415c5cb0405ee43b17f532c075ae7a93d9ff
Daniel Wagener <daniel.j.wagener@gmail.com>
2020-01-18 17:19:52 -0500

    Render user's name on dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,7 +5,12 @@ const Dashboard = props => {
   if (!props.auth) {
     return <h1>You are not logged in!</h1>;
   }
-  return <h1>DASHBOARD</h1>;
+
+  if (props.auth.givenName) {
+    return <h1>{`Welcome, ${props.auth.givenName}!`}</h1>;
+  } else {
+    return <h1>Welcome!</h1>;
+  }
 };
 
 function mapStateToProps({ auth }) {
