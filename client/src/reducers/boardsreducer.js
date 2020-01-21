6b35402b713aef8a40df4f61520c72b6c69fce3b
Daniel Wagener <daniel.j.wagener@gmail.com>
2020-01-21 13:19:29 -0500

    Load user's board into Redux on login, dump on logout

--- client/src/reducers/boardsreducer.js
+++ client/src/reducers/boardsreducer.js
@@ -1,7 +1,12 @@
-import { ADD_BOARD } from "../actions/types";
+import { ADD_BOARD, FETCH_USER } from "../actions/types";
 
 export default (state = [], action) => {
   switch (action.type) {
+    case FETCH_USER:
+      if (action.payload) {
+        return [...action.payload.boards];
+      }
+      return state;
     case ADD_BOARD:
       return [...action.payload.boards];
     default:
