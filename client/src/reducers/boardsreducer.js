ff238b272bcc2bea77f730c2fafd5fac1b2de921
Daniel Wagener <daniel.j.wagener@gmail.com>
2020-01-25 10:59:14 -0500

    Direct reducer only to load boards if boards exist

--- client/src/reducers/boardsreducer.js
+++ client/src/reducers/boardsreducer.js
@@ -3,7 +3,7 @@ import { ADD_BOARD, FETCH_USER } from "../actions/types";
 export default (state = [], action) => {
   switch (action.type) {
     case FETCH_USER:
-      if (action.payload) {
+      if (action.payload.boards) {
         return [...action.payload.boards];
       }
       return state;
