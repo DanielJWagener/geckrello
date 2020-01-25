84fa9e76d9e5420c91dcf8ba91d5e97ba80c418b
Daniel Wagener <daniel.j.wagener@gmail.com>
2020-01-25 11:14:09 -0500

    Render a board link to screen for each of the user's boards

---
+++ client/src/components/dashboard/BoardLink.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BoardLink = props => {
+  return <h1>I'm a board link</h1>;
+};
+
+export default BoardLink;
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,16 +2,24 @@ import React from "react";
 import { connect } from "react-redux";
 
 import AddBoard from "./AddBoard";
+import BoardLink from "./BoardLink";
 
 const Dashboard = props => {
   if (!props.auth) {
     return <h1>You are not logged in!</h1>;
   }
 
+  const renderBoards = () => {
+    return props.boards.map(board => {
+      return <BoardLink key={board} />;
+    });
+  };
+
   if (props.auth.givenName) {
     return (
       <>
         <h1>{`Welcome, ${props.auth.givenName}!`}</h1>
+        {renderBoards()}
         <AddBoard />
       </>
     );
@@ -20,8 +28,8 @@ const Dashboard = props => {
   }
 };
 
-function mapStateToProps({ auth }) {
-  return { auth };
+function mapStateToProps({ auth, boards }) {
+  return { auth, boards };
 }
 
 export default connect(mapStateToProps)(Dashboard);
