3c3406b8dd2fc6e9f48e13ab733d4a29005b0910
Daniel Wagener <daniel.j.wagener@gmail.com>
2020-01-31 20:24:07 -0500

    Move cards using modal

--- client/src/components/board/list/card/SideMenu.js
+++ client/src/components/board/list/card/SideMenu.js
@@ -55,7 +55,7 @@ class SideMenu extends React.Component {
     this.props.lists
       .filter(list => !list.archived)
       .map(list => (
-        <option key={list.listId} value={list.listId}>
+        <option key={list._id} value={list._id}>
           {list.title}
         </option>
       ));
