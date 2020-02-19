0c2fd4ae1a0089c362edf9e8fe2c47c55c21fc0e
DanielJWagener <daniel.j.wagener@gmail.com>
2020-02-18 19:41:02 -0500

    Refactor Change Background component

--- client/src/components/menu-bar/ChangeBackground.js
+++ client/src/components/menu-bar/ChangeBackground.js
@@ -1,24 +1,14 @@
 import React from "react";
 import ChangeBackgroundInner from "./ChangeBackgroundInner.js";
 
-const ChangeBackground = props => {
-  if (props.hidden) {
-    return (
-      <div className="sidebar-menu__interface sidebar-menu__interface--hidden">
-        <ChangeBackgroundInner
-          toggleChangeBackground={props.toggleChangeBackground}
-        />
-      </div>
-    );
-  } else {
-    return (
-      <div className="sidebar-menu__interface sidebar-menu__interface--visible">
-        <ChangeBackgroundInner
-          toggleChangeBackground={props.toggleChangeBackground}
-        />
-      </div>
-    );
-  }
-};
+const ChangeBackground = ({ hidden, toggleChangeBackground }) => (
+  <div
+    className={`sidebar-menu__interface sidebar-menu__interface--${
+      hidden ? "hidden" : "visible"
+    }`}
+  >
+    <ChangeBackgroundInner toggleChangeBackground={toggleChangeBackground} />
+  </div>
+);
 
 export default ChangeBackground;
