32b1d2fd69cdf0723037a90f12c43016a5c86d4f
DanielJWagener <daniel.j.wagener@gmail.com>
2020-02-18 19:38:23 -0500

    Refactor archive component

--- client/src/components/menu-bar/Archive.js
+++ client/src/components/menu-bar/Archive.js
@@ -1,20 +1,14 @@
 import React from "react";
 import ArchiveInner from "./ArchiveInner";
 
-const Archive = props => {
-  if (props.hidden) {
-    return (
-      <div className="sidebar-menu__interface sidebar-menu__interface--hidden">
-        <ArchiveInner toggleArchive={props.toggleArchive} />
-      </div>
-    );
-  } else {
-    return (
-      <div className="sidebar-menu__interface sidebar-menu__interface--visible">
-        <ArchiveInner toggleArchive={props.toggleArchive} />
-      </div>
-    );
-  }
-};
+const Archive = ({ hidden, toggleArchive }) => (
+  <div
+    className={`sidebar-menu__interface sidebar-menu__interface--${
+      hidden ? "hidden" : "visible"
+    }`}
+  >
+    <ArchiveInner toggleArchive={toggleArchive} />
+  </div>
+);
 
 export default Archive;
