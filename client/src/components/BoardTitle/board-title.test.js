60d3e35a102cffce6dc90303f2485a1a6b0b2023
DanielJWagener <daniel.j.wagener@gmail.com>
2020-04-04 18:36:56 -0400

    Update BoardTitle tests

--- client/src/components/BoardTitle/board-title.test.js
+++ client/src/components/BoardTitle/board-title.test.js
@@ -1,11 +1,24 @@
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import React from "react";
 import BoardTitle from "./board-title.component";
 import { BoardTitle as BoardTitlePure } from "./board-title.component";
 import Root from "../../Root";
 
+const mockProps = {
+  board: {
+    title: "title",
+  },
+};
+
 it("renders BoardTitle component", () => {
-  expect(
-    shallow(<BoardTitlePure board={{ title: "title" }} />).debug()
-  ).toMatchSnapshot();
+  expect(shallow(<BoardTitlePure {...mockProps} />).debug()).toMatchSnapshot();
+});
+
+it("has an input users can type into", () => {
+  const wrapper = mount(<BoardTitlePure {...mockProps} />);
+  wrapper.find("input").simulate("change", {
+    target: { value: "new title" },
+  });
+  wrapper.update();
+  expect(wrapper.find("input").prop("value")).toEqual("new title");
 });
