7e28879ba46d25337d90ceb78bfc423227c1eed4
DanielJWagener <daniel.j.wagener@gmail.com>
2020-04-04 21:18:53 -0400

    Remove obselete SCSS import

--- client/src/components/BoardMenuContent/board-menu-item.component.jsx
+++ client/src/components/BoardMenuContent/board-menu-item.component.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import "./board-menu-item.styles.scss";
-
 const BoardMenuItem = ({ togglePanel, toggleTarget, children }) => (
   <button className="boardmenu__item" onClick={() => togglePanel(toggleTarget)}>
     {children}
